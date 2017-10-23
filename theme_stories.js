c1e7e752830430d33272d5ffc530b6d300302c63
jessier08 <jessierichards@Jessies-MacBook-Pro-2.local>
2017-10-23 15:12:29 -0400

    final update to theme stories
    added link_to_page with unique ID to generate for single node page link
    on the story_visual div, finished mobile, fixed css classes, deleted
    the large single node video files

--- theme_stories.js
+++ theme_stories.js
@@ -19,19 +19,22 @@ var data = [
                     story: "Huskies legend Jack Grinold creates a legacy that lives on through the student-athletes who benefit from the scholarship his family established.",
                     theme_quote: "Like Jack, I strive to put Northeastern Men's Rowing and Northeastern University ahead of myself, and I have dedicated my collegiate career to try to make the team the best it has ever been.",
                     quote_attr: "Trevor Appier, BHS'18",
-                    video: "story_videos/trevor.mp4"
+                    video: "story_videos/trevor.mp4",
+                    link_to_page:"Person85"
                 },
                 {headline: "Scoring a Competitive Advantage",
                     story:"Jumpstarted with a $1 million gift from William Shea, LA'70, MA'72, alumni establish the Fernie Flaman Endowed Men's Ice Hockey Coach, one of only two endowed coaching positions in Hockey East.",
                     theme_quote: "Fernie’s leadership established a tradition of hard work, good character, and athletic accomplishment. A strong hockey program provides yet another avenue for our students to achieve excellence.",
                     quote_attr: "James Madigan, DMSB'86",
-                    video:"story_videos/james_madigan.mp4"
+                    video:"story_videos/james_madigan.mp4",
+                    link_to_page:"Person1366"
                 },
                 {headline:"Athlete Honors a Family's Memory",
                     story:"Maggie Walsh, DMSB'18, never imagined she would be captain of Women's Rowing, but her athletic aspirations have been made possible by the Kristen Elizabeth Prendiville Memorial Scholarship.",
                 theme_quote:"Since being at Northeastern, I have rowed in 4 Head of the Charles Regattas, presented to executives of Fortune 100 companies, been coached by an Olympian, and traveled to South Africa for field research in my major. Please know that I would not be saying any of these things without the incredible generosity and support of the Prendiville family.",
                 quote_attr:"Maggie Walsh, DMSB'18",
-                 video:"story_videos/maggie.mp4"
+                 video:"story_videos/maggie.mp4",
+                 link_to_page:"Person243"
                 }
             ]
         },
@@ -42,21 +45,24 @@ var data = [
                     story:"Northeastern students learn the art and science of philanthropy from Rebecca Riccio, whose leadership of the Social Impact Lab teaches strategic philanthropy worldwide.",
                     theme_quote:"Our goal is to give students a comprehensive toolkit for translating good intentions into effective, financially sustainable strategies for achieving social change.",
                     quote_attr:"Rebecca Riccio",
-                    video:"story_videos/rebecca.mp4"
+                    video:"story_videos/rebecca.mp4",
+                    link_to_page:"Person1852"
                 },
                 {
                     headline:"Finding a Voice and Cheering a Team",
                     story: "Max Plansky, a teacher with cerebal palsy, enjoys his role with the Northeastern men's basketball team while benefiting from tools made possible by philanthropy, including a new voice.",
                     theme_quote:"In 2013, Max became part of Northeastern University’s men’s basketball team. These players welcomed him as their teammate. His life changed — and so did theirs.",
                     quote_attr:"Michael Plansky, Max's father",
-                    video:"story_videos/max_plansky.mp4"
+                    video:"story_videos/max_plansky.mp4",
+                    link_to_page:"Person1761"
                 },
                 {
                     headline:"For Those Who Serve Us",
                     story:"Support for Northeastern's Center for the Advancement of Veterans and Servicemembers allows student-veterans like Sarah Nelson, S'17, to learn, grow, and thrive.",
                     quote:"Coming to Northeastern was a no-brainer. The assistance to veterans, both financial and in terms of services, is remarkable.",
                     quote_attr:"Sarah Nelson, S'17",
-                    video:"story_videos/sarah_nelson.mp4"
+                    video:"story_videos/sarah_nelson.mp4",
+                    link_to_page:"Person1611"
                 }
             ]
         },
@@ -67,14 +73,16 @@ var data = [
                     story:"At the student-run Collegiate Alternative Investment Summit, generously funded by the DMSB Dean's Fund, students learn to grow their assets both professionally and literally.",
                     quote:"The success and impact of CAIS would not be possible without the unwavering generosity of our alumni, who have repeatedly shown their commitment to the students of Northeastern by providing their support in ways I never would have imagined.",
                     quote_attr:"Colby Gilbert, DMSB'20",
-                    video:"story_videos/cais.mp4"
+                    video:"story_videos/cais.mp4",
+                    link_to_page:"NonPerson66"
                 },
                 {
                     headline:"Room to Grow Engineers",
                     story:"Addressing a need for space for meeting, making, and mentorship, support from the COE Dean's Fund creates a first-year learning center for engineering students. The result: design blossoming in a collaborative space, as exemplified by Christopher Scianna, E'19.",
                     quote:"In my experience, the first year learning center is the epicenter for the design process. The central theme for my cornerstone project was service learning, specifically an after school outreach program at Orchard Gardens K-8 school focused on youth engineering exposure. The FYELC soon became our home base for this project, turning it into the backdrop for weekly planning sessions with Professor Freeman where we created design challenges and discussed lesson plans.",
                     quote_attr:"Christopher Scianna, E'19",
-                    video:"story_videos/FYELIC.mp4"
+                    video:"story_videos/FYELIC.mp4",
+                    link_to_page:"NonPerson152"
                 }
             ]
         },
@@ -85,21 +93,24 @@ var data = [
                     story:"Northeastern students Ali Kothari, DMSB'17, and Johnny Fayad, DMSB'17, have built a successful startup venture thanks to donor support and mentorship from Northeastern's robust entrepreneurial ecosystem.",
                     theme_quote:"When I’m not out selling CoffeeBars, I’m still doing everything I can to stay involved around campus, supporting the next wave of student entrepreneurs at Northeastern.",
                     quote_attr:"Johnny Fayad, DMSB'17",
-                    video:"story_videos/EYC.mp4"
+                    video:"story_videos/EYC.mp4",
+                    link_to_page:"NonPerson137"
                 },
                 {
                     headline:"Opening Doors Through Scholarships",
                     story: "Two student entrepreneurs supported by two separate scholarships work together to launch one startup that will change lives.  ScholarJet is the hands-on tool that provides students with action-based scholarships.",
                     theme_quote:"I stepped foot onto the Northeastern campus thinking that I would become a great engineer. However, because of Northeastern, I learned that I can also be an entrepreneur. Because of your gift, ScholarJet may one day change the world.",
                     quote_attr:"Tuan Ho, E'18",
-                    video:"story_videos/scholarjet.mp4"
+                    video:"story_videos/scholarjet.mp4",
+                    link_to_page:"NonPerson353"
                 },
                 {
                     headline:"Acting Locally and Globally",
                     story:"Already a leader in the Boston community, Mabel Gonzalez Nunez, DMSB'20, works with the Social Enterprise Institute to make an impact on a global scale.",
                     theme_quote:"Thank you for instilling in me a drive to empower those who did not win the 'lottery of life.'",
                     quote_attr:"Mabel Gonzalez Nunez, DMSB'20",
-                    video:"story_videos/mabel.mp4"
+                    video:"story_videos/mabel.mp4",
+                    link_to_page:"Person863"
                 }
             ]
         },
@@ -110,21 +121,24 @@ var data = [
                     story:"Inspired by the visionary philanthropy of Rich D'Amore and Alan McKim, Gary and Lea Anne Dunton establish the first-ever endowed, named deanship at the university.",
                     theme_quote:"It is an amazing honor to have the opportunity to lead the D’Amore-McKim School of Business.",
                     quote_attr:"Raj Echambadi",
-                    video:"story_videos/raj.mp4"
+                    video:"story_videos/raj.mp4",
+                    link_to_page:"Person667"
                 },
                 {
                     haedline:"Strengthening Global Relations",
                     story:"Support for Professor Denise Garcia's work in security studies, disarmament, climate change, and international relations inspires Northeastern's most driven global scholars and leaders of tomorrow.",
                     theme_quote:"This generosity entirely transformed my research life, which aims to be a transformative force in world politics.",
                     quote_attr:"Denise Garcia",
-                    video:"story_videos/denise.mp4"
+                    video:"story_videos/denise.mp4",
+                    link_to_page:"Person802"
                 },
                 {
                     headline:"The Founding Father of Entrepreneurship",
                     story:"Retired faculty Dan McCarthy mentored countles students in entrepreneurship, including Rich D'Amore and Alan McKim.  Then he paired with alum Jeff McCarthy on his own venture--in philanthropy.",
                     theme_quote:"In business, there’s a long tradition of altruism toward the next generation.",
                     quote_attr:"Daniel McCarthy",
-                    video:"story_videos/mccarthy.mp4"
+                    video:"story_videos/mccarthy.mp4",
+                    link_to_page:"Person1435"
                 }
             ]
         },
@@ -135,14 +149,16 @@ var data = [
                     story:"Gifts to Northeastern's Ocean Genome Legacy enable the world's leading scientists to uncover the ocean's deepest mysteries, and to support research that cures disease and protects the environment.",
                     quote:"We've seen a lot of really exciting and interesting things come out of the study of the genomes of the terrestrial world. If we expand that to the extreme diversity of the marine environment, it's almost unimaginable what might come of it.",
                     quote_attr:"Daniel Distel",
-                    video:"story_videos/ocean.mp4"
+                    video:"story_videos/ocean.mp4",
+                    link_to_page:"NonPerson290"
                 },
                 {
                     headline:"Co-ops That Shape Careers",
                     story:"Co-ops at the George J. Kostas Research Institute for Homeland Security provide the platform upon which careers flourish.",
                     theme_quote:"The work I did at KRI shaped my undergraduate experience. It informed my plans to work and study abroad, as well as my post-graduate professional and academic ambitions.",
                     quote_attr:"Connor Goddard, SSH'14",
-                    video:"story_videos/kostas.mp4"
+                    video:"story_videos/kostas.mp4",
+                    link_to_page:"NonPerson212"
                 }
             ]
         },
@@ -153,28 +169,32 @@ var data = [
                     story:"A visionary investment from Bernie Gordon, H'07, pays dividends for students like Maddy Leger, E'19, and Gordon Center for Engineering Leadership Founding Director Michael Silevitch.",
                     quote:"I am honored to be a part of what your legacy at Northeastern is providing!",
                     quote_attr:"Maddy Leger, E'19",
-                    video:"story_videos/maddy.mp4"
+                    video:"story_videos/maddy.mp4",
+                    link_to_page:"Person1264"
                 },
                 {
                     headline:"Paying It Forward",
                     story:"Lamar Cardinez, DMSB'14, received both the Ujima and Samuel and Nancy Altschuler Scholarships at Northeastern. Now, he and his wife, Italia, DMSB14, pay it forward by supporting the Torch Scholarship Program.",
                     theme_quote:"I was blessed to receive scholarships during my time at Northeastern. I am forever humbled by the extreme generosity exhibited by the donors who had a direct impact on my life during and after college. This generosity is what led my wife, Italia, and I to start our own scholarship fund to help students get access to the technological tools needed for college.",
                     quote_attr:"Lamar Cardinez, DMSB'14",
-                    video:"story_videos/lamar.mp4"
+                    video:"story_videos/lamar.mp4",
+                    link_to_page:"Person347"
                 },
                 {
                     headline:"Crossing Boundaries",
                     story:"A gift of financial aid in the School of Law branches out when student Kacy Cuenta, L'17, advises entrepreneur Abbey Titcomb, E'18, on intellectual property rights to help Titcomb launch Knightly, an integrated safety network.",
                     theme_quote:"I wanted to pursue my law degree at a place where there was both camaraderie and opportunity for practical hands-on experience that would prepare me to practice in the real world. Working with Abbey through the IP CO-LAB was a highlight of my law school experience.",
                     quote_attr:"Kacy Cuenta, L'17",
-                    video:"story_videos/kacy.mp4"
+                    video:"story_videos/kacy.mp4",
+                    link_to_page:"Person510"
                 },
                 {
                     headline:"A Long Road to Success",
                     story:"The Howard W. Evirs, Jr., Scholarship made it possible for Lisa Mucciarone, CPS'17, to graduate after family difficulties and financial strain put her on a non-traditional path to a degree. It also inspired another member of the Evirs family to help others pursue their goals.",
                     quote:"I was a single mother of three, living paycheck to paycheck, determined to finish the degree I began in 2009. Thanks to the generosity of Howard and Helen Evirs, as well as other donors who support financial aid, I am proud to say that I graduated in 2017. One day, I hope to give back and help other students pursue their dreams.",
                     quote_attr:"Lisa Mucciarone, CPS'17",
-                    videos:"story_videos/lisa.mp4"
+                    videos:"story_videos/lisa.mp4",
+                    link_to_page:"Person1566"
                 }
             ]
         }
